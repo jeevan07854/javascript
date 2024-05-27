8f742cf630c2baae122962cb776958985ce36774
Jeevan Poudel <jeevan1.785401@cct.tu.edu.np>
2024-05-27 23:01:55 +0545

    promise

--- Dominjs.js
+++ Dominjs.js
@@ -1,2 +1,22 @@
-console.log("hello world!")
-console.log("Helloo how")
+// console.log("hello world!");
+// console.log("Helloo how");
+
+
+
+const promiseone = new Promise((resolve,reject)=>{
+setTimeout(()=>{
+    const error=true;
+    if (!error){
+        return resolve();
+    }
+    else{
+        return reject();
+    }
+},2000)
+
+});
+promiseone.then(()=>{
+console.lor("RESOLVE");
+}).catch(()=>{
+    console.log("Resolve");
+})
